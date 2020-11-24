9951a062999651bc7bb7958ce2a7f8b820c3c252
Débora <deboracristinapereira123@gmail.com>
2020-11-23 23:39:46 -0300

    Operacoes basicas - JS

---
+++ scripts/script.js
@@ -0,0 +1,94 @@
+// Inputs numbers
+
+var numberA = document.querySelector("#numberA");
+var numberB = document.querySelector("#numberB");
+
+// Outros Inputs
+
+var sum = document.querySelector("#sum");
+var sub1 = document.querySelector("#sub1");
+var sub2 = document.querySelector("#sub2");
+var multiplication = document.querySelector("#multiplication");
+var div1 = document.querySelector("#div1");
+var div2 = document.querySelector("#div2");
+var squaredA = document.querySelector("#squaredA");
+var squaredB = document.querySelector("#squaredB");
+var dividersA = document.querySelector("#dividersA");
+var dividersB = document.querySelector("#dividersB");
+var factorialA = document.querySelector("#factorialA");
+var factorialB = document.querySelector("#factorialB");
+
+function start() {
+  getNumberA();
+  getNumberB();
+}
+
+start();
+
+// Pegar valor quando usuário apertar enter
+
+function getNumberA() {
+  window.addEventListener('keyup', getValueA);
+
+  function getValueA(event) {
+    if (event.key === "Enter") {
+      console.log(numberA.value)
+    }
+  }
+}
+
+function getNumberB() {
+  window.addEventListener('keyup', getValueB);
+
+  function getValueB(event) {
+    if (event.key === "Enter") {
+      
+      // Chamar funções
+
+      sumNumbers(numberA.value, numberB.value);
+      subtration1Numbers(numberA.value, numberB.value);
+      subtration2Numbers(numberA.value, numberB.value);
+      multipleNumbers(numberA.value, numberB.value);
+      division1Numbers(numberA.value, numberB.value);
+      division2Numbers(numberA.value, numberB.value);
+    }
+  }
+}
+
+function sumNumbers(numberA, numberB) {
+
+  // String para int
+
+  sum.value = parseInt(numberA) + parseInt(numberB);
+}
+
+function subtration1Numbers(numberA, numberB) {
+
+  sub1.value = parseInt(numberA) - parseInt(numberB);
+
+}
+
+function subtration2Numbers(numberA, numberB) {
+
+  sub2.value = parseInt(numberB) - parseInt(numberA);
+
+}
+
+function multipleNumbers(numberA, numberB) {
+
+  multiplication.value = parseInt(numberA) * parseInt(numberB);
+
+}
+
+function division1Numbers(numberA, numberB) {
+
+  div1.value = parseInt(numberA) / parseInt(numberB);
+
+}
+
+function division2Numbers(numberA, numberB) {
+
+  div2.value = parseInt(numberB) / parseInt(numberA);
+
+  // TO DO: toFixed(2)
+}
